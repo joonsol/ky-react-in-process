28e145c80801e35009b7dee70422828b5ca19374
joonsol <71648638+joonsol@users.noreply.github.com>
2025-08-06 12:44:00 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 
 import './App.css'
-import Section from './components/Section'
+import Button from './components/Button'
+import Input from './components/Input'
 function App() {
-  
+
 
   return (
     <div>
-      <Section/>
+      <Input
+        inputValue={'hello react'}
+        title={'input title'}
+        placeholder={'입력하세요'} />
       <h1>hello React!</h1>
+      <Button text={'메일'} color={'red'} />
+      <Button text={'카페'} color={'blue'} />
+      <Button text={'블로그'} color={'orange'} />
+      <Button text={'기본값 설정'} />
+      <Button text={'자식요소 설정'} color={'pink'}>
+        <strong>중요</strong>
+      </Button>
     </div>
   )
 }
