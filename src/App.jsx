a6000d7ada49b761cb4c9996cc466d2282ddab8a
joonsol <71648638+joonsol@users.noreply.github.com>
2025-08-07 14:24:02 +0900

    0

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 
 import './App.css'
-import FocusInput from './components/FocusInput'
-import Counter from './components/Counter'
-import InputTracker from './components/InputTracker'
-
+import LifeCycle from './components/LifeCycle'
 function App() {
 
 
 
   return (
     <div>
-      <InputTracker/>
-      <Counter/>
-      <FocusInput/>
+      <LifeCycle/>
+
     </div>
   )
 }
