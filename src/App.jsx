230cf9922137843c1def6da8408dcb0dca911ee5
joonsol <71648638+joonsol@users.noreply.github.com>
2025-08-07 14:12:23 +0900

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 
 import './App.css'
-import Exam1 from './components/Exam1'
-import Exam2 from './components/Exam2'
-import Exam3 from './components/Exam3'
+import FocusInput from './components/FocusInput'
+import Counter from './components/Counter'
+import InputTracker from './components/InputTracker'
+
 function App() {
 
-  const user={
-    name:'Alice',
-    age:21
-  }
+
 
   return (
     <div>
-      <Exam1 {...user}/>
-      <Exam2/>
-      <Exam3/>
+      <InputTracker/>
+      <Counter/>
+      <FocusInput/>
     </div>
   )
 }
---
+++ src/components/FocusInput.jsx
@@ -0,0 +1,20 @@
+import React ,{useRef} from 'react'
+
+const FocusInput = () => {
+    const inputRef = useRef(null)
+
+
+const focusInput=()=>{
+    inputRef.current.focus()
+}
+
+
+  return (
+    <div>
+        <input type="text" ref={inputRef} placeholder='클릭하면 포커스됨'/>
+        <button onClick={focusInput}>포커스</button>
+    </div>
+  )
+}
+
+export default FocusInput
