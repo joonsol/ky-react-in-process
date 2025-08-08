e93c233f93bf3e3ae3a812855c94774fb1dad702
joonsol <71648638+joonsol@users.noreply.github.com>
2025-08-08 14:50:34 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 
 import './App.css'
-import SquareCalc from './components/SquareCalc'
-import SortableList from './components/SortableList'
+import UserList from './components/UserList'
 function App() {
 
 
   return (
     <div>
-      <SortableList/>
-      {/* <SquareCalc/> */}
+      <UserList/>
     </div>
   )
 }
